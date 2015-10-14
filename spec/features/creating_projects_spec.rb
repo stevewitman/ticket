31c26bcb2831fa529c3e2d13412cb8877849d052
Steve Witman <stevewitman@gmail.com>
2015-10-13 23:18:27 -0600

    installed rspec

---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new projects" do
+  scenario "with valid attributes" do
+    visit "/"
+    click_link "New Project"
+    fill_in "Name", with: "Title of project 1"
+    fill_in "Description", with: "Some description"
+    click_button "Create Project"
+    expect(page).to have_content "Project has been created."
+  end
+end
