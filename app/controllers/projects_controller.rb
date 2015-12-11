8c6d21345724b3062d70baccd9b8a0e07a915632
Steve Witman <stevewitman@gmail.com>
2015-12-10 23:35:55 -0700

    create action for Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,18 @@ class ProjectsController < ApplicationController
   def new
   	@project = Project.new
   end
+
+  def create 
+  	@project = Project.new(project_params)
+
+  	if @project.save
+  		redirect_to @project, notice: "Project has been created."
+  	else
+  		# nothing yet
+  	end
+  end
+
+  def project_params
+  	params.require(:project).permit(:name, :description)
+  end
 end
