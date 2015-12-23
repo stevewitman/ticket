fe26e61bcf4620521dbb50f5100c7d8be473dbaa
Steve Witman <stevewitman@gmail.com>
2015-12-22 23:08:03 -0700

    added delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -38,6 +38,14 @@ class ProjectsController < ApplicationController
   	@project = Project.find(params[:id])
   end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
+
   def project_params
   	params.require(:project).permit(:name, :description)
   end
