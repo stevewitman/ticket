12ce33429e824d25a59a8bc45b6ce6367d5a7545
Steve Witman <stevewitman@gmail.com>
2015-12-24 00:04:09 -0700

    redirect user to projects if visiting invalid url

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -20,11 +22,11 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
+    # @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
+    # @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -35,14 +37,23 @@ class ProjectsController < ApplicationController
   end
 
   def show
-  	@project = Project.find(params[:id])
+  	# @project = Project.find(params[:id])
   end
 
   def destroy
-    @project = Project.find(params[:id])
+    # @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
+
+private
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
     redirect_to projects_path
   end
 
@@ -50,3 +61,5 @@ class ProjectsController < ApplicationController
   	params.require(:project).permit(:name, :description)
   end
 end
+
+
